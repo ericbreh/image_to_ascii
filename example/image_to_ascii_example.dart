993041e6907cc4d84d29f075c64282013d41dced
Eric Chuang <ericchuang94@gmail.com>
2025-05-18 23:55:16 -0700

    basic ascii conversion

--- example/image_to_ascii_example.dart
+++ example/image_to_ascii_example.dart
@@ -1,6 +1,5 @@
 import 'package:image_to_ascii/image_to_ascii.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  convertImageToAscii("../test/eko.png");
 }
--- lib/src/image_to_ascii_base.dart
+++ lib/src/image_to_ascii_base.dart
@@ -1,6 +1,40 @@
-// TODO: Put public facing types in this file.
+import 'dart:io';
+import 'package:image/image.dart';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+void convertImageToAscii(String imagePath) {
+  try {
+    // Load the image
+    final bytes = File(imagePath).readAsBytesSync();
+    final image = decodeImage(bytes);
+
+    if (image == null) {
+      print('Could not decode image');
+      return;
+    }
+
+    // Resize image
+    final resizedImage = copyResize(image, width: 100);
+
+    // ASCII character set from darkest to lightest
+    final asciiChars = '@%#*+=-:. ';
+
+    // Convert each pixel to an ASCII character
+    for (int y = 0; y < resizedImage.height; y++) {
+      String line = '';
+      for (int x = 0; x < resizedImage.width; x++) {
+        final pixel = resizedImage.getPixel(x, y);
+
+        // Calculate grayscale value
+        final grayscale =
+            (getRed(pixel) + getGreen(pixel) + getBlue(pixel)) ~/ 3;
+
+        // Map grayscale value to ASCII character
+        final index = ((grayscale / 255) * (asciiChars.length - 1)).round();
+        line += asciiChars[index];
+      }
+      print(line);
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
 }
