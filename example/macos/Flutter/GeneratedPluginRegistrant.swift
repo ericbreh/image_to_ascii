9e9a7aeeb3bb02dcf200643d3380af33304a90f5
Eric Chuang <ericchuang94@gmail.com>
2026-06-10 21:54:09 -0700

    add camera desktop package

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import camera_desktop
 import file_selector_macos
-import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CameraDesktopPlugin.register(with: registry.registrar(forPlugin: "CameraDesktopPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
